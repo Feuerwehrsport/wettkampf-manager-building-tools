3b5cfaa198de24b60d418a0cc3538d6f639cec47
Georg Limbach <glimbach@milabent.com>
2015-06-24 22:58:25 +0200

    Installation erweitern

--- install.rb
+++ install.rb
@@ -1,13 +1,24 @@
+require 'fileutils'
+
 def absolute *path_args
   File.join(File.dirname(__FILE__), *path_args)
 end
 
 if ARGV[0] == "first"
-  puts "first"
+  puts "Neuladen des Quellcodes (Teil 1)"
   system("ruby", "#{absolute("update_code.rb")}", out: $stdout, err: :out)
   system("ruby", "#{absolute("install.rb")}", out: $stdout, err: :out)
 else
-  puts "second"
+  puts "Neuladen des Quellcodes (Teil 2)"
   system("ruby", "#{absolute("update_code.rb")}", out: $stdout, err: :out)
-  puts "do some other stuff"
+
+  Dir.chdir "wettkampf-manager-master"
+  system("bundle", out: $stdout, err: :out)
+  FileUtils.rm_rf("public/assets")
+
+  production = { "RAILS_ENV" => "production" }
+  system(production, "bundle", "exec rake assets:precompile", out: $stdout, err: :out)
+  system(production, "bundle", "exec rake db:migrate", out: $stdout, err: :out)
+  system(production, "bundle", "exec rake db:seed", out: $stdout, err: :out)
+  system(production, "bundle", "exec rake import_suggestions", out: $stdout, err: :out)
 end
