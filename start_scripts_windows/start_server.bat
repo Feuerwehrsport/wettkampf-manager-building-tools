e72373f95d43cb9c471811029c00c718b098d31f
Georg Limbach <glimbach@milabent.com>
2015-06-25 12:35:43 +0200

    Compiler-Suite in Path aufnehmen

--- start_scripts_windows/start_server.bat
+++ start_scripts_windows/start_server.bat
@@ -4,7 +4,7 @@ PUSHD %~dp0.
 SET ROOT_DIR=%CD%
 POPD
 
-SET PATH=%ROOT_DIR%\Ruby2.1.0\bin;%ROOT_DIR%\Ruby2.1.0\lib\ruby\gems\2.1.0\bin;%ROOT_DIR%\DevKit\bin;%PATH%
+SET PATH=%ROOT_DIR%\Ruby2.1.0\bin;%ROOT_DIR%\Ruby2.1.0\lib\ruby\gems\2.1.0\bin;%ROOT_DIR%\DevKit\bin;%ROOT_DIR%\DevKit\mingw\bin;%PATH%
 
 ruby wettkampf-manager-building-tools-master/start_server.rb first
 
