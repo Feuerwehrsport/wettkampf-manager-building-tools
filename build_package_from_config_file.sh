0fea140afcfa6bfc5d37ba3d69f6dce9f4fc9a76
Georg Limbach <georg.limbach@lichtbit.com>
2017-03-28 15:17:22 +0200

    Altdaten entfernen

--- build_package_from_config_file.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CONFIG_FILE=/home/georf/.wettkampf-manager/config
-
-if [ ! -f "$CONFIG_FILE" ] ; then exit 1 ; fi
-
-source "$CONFIG_FILE"
-
-/home/georf/wettkampf-manager-building-tools/package_builder/build.sh -v "$VERSION" -g "$GIT_COMMIT_ID" -d "$DATE" -c "$CHANGE_FILE" -f
