0cf0b2fee70c6cba6827efbb9832d8759e8e710c
Georg Limbach <georg.limbach@lichtbit.com>
2020-08-20 12:50:08 +0200

    Pfad auf spezielle Zeichen prüfen

--- package_builder/windows/Skript_backup_im_hintergrund.bat
+++ package_builder/windows/Skript_backup_im_hintergrund.bat
@@ -4,6 +4,19 @@ PUSHD %~dp0.
 SET ROOT_DIR=%CD%
 POPD
 
+SETLOCAL EnableExtensions EnableDelayedExpansion
+
+>"%temp%\wettkampfmanagerpath.txt" echo(!ROOT_DIR!
+findstr /r ".*[()<>\"\"%%].*" "%temp%\wettkampfmanagerpath.txt" >nul
+if %errorlevel% equ 0 (
+  goto :patherror
+)
+findstr /r /c:"[ ]" "%temp%\wettkampfmanagerpath.txt" >nul
+if %errorlevel% equ 0 (
+  goto :patherror
+)
+ENDLOCAL
+
 SET PATH=%ROOT_DIR%\ruby\bin;%ROOT_DIR%\ruby\lib\ruby\gems\2.6.0\bin;%PATH%
 SET RAILS_ENV=production
 
@@ -12,3 +25,21 @@ bundle exec rake backup_data_recurring
 POPD
 
 PAUSE
+goto :eof
+
+:patherror
+
+echo.
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo.
+echo    Der Pfad darf keine Leerzeichen oder andere Sonderzeichen enthalten.
+echo           Aktueller Pfad:
+echo           %ROOT_DIR%
+echo.
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+echo.
+PAUSE
+
+goto :eof
